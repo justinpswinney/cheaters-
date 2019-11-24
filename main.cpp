4c58159d9dd5af596d2f07d8660e2a145f69b52b
justinpswinney <justinswinney@utexas.edu>
2019-11-24 14:15:57 -0600

    Started file I/O
    Copied over Priebe's file name function and made it part of a higher level main that we'll be making later on.

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int getFileNames(vector<string> &files);
+
+int main(){
+    vector<string> files;
+    getFileNames(files);
+    for(std::vector<string>::iterator i=files.begin();i!=files.end();i++){
+        cout << *i << endl;
+    }
+    return 0;
+}
+
+
+
+/*function... might want it in some class?*/
+int getdir (string dir, vector<string> &files)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        files.push_back(string(dirp->d_name));
+    }
+    closedir(dp);
+    return 0;
+}
+
+int getFileNames(vector<string> &files)
+{
+    string dir = string("sm_doc_set");
+    files = vector<string>();
+
+    getdir(dir,files);
+
+    for (unsigned int i = 0;i < files.size();i++) {
+        cout << i << files[i] << endl;
+    }
+    return 0;
+}
